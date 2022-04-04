04b8e5ec8974892830ea79af12a888d2c9149106
Aleksandr Butakov <anbngm@gmail.com>
2022-04-04 10:24:50 +0500

    Changed test

--- src/test/java/cloud/autotests/tests/GeneratedTests.java
+++ src/test/java/cloud/autotests/tests/GeneratedTests.java
@@ -16,44 +16,92 @@ public class GeneratedTests extends TestBase {
     @Description("Soon to be implemented by you (or QA.GURU engineers)")
     @DisplayName("Autotest_RSHB")
     void generatedTest() {
+
         step("- Зайти на сайт rshb.ru", () -> {
-            step("// todo: just add selenium action");
+            open("https://rshb.ru");
         });
 
         step("- Кликнуть «Частным лицам»", () -> {
-            step("// todo: just add selenium action");
+            $(".header__section-navigation").$(byText("Частным лицам")).click();
         });
 
         step("- Кликнуть «Кредиты»", () -> {
-            step("// todo: just add selenium action");
+            $(".b-page-head").$(byText("Кредиты")).click();
         });
 
-        step("- Кликнуть «Потребительский кредит без обеспечения»", () -> {
-            step("// todo: just add selenium action");
+        step("- Кликнуть «Кредит на любые цели»", () -> {
+            //$(".linkip").$(byText("Кредит на любые цели")).click();
+            $(".container").$(byText("Кредит на любые цели")).click();
         });
 
-        step("- Расчитать кредит", () -> {
-            step("// todo: just add selenium action");
+        step("- Закрыть окно cookies", () -> {
+            $(".cookie-consent__submit-button").click();
         });
 
         step("- тип платежа «Аннуитентный»", () -> {
-            step("// todo: just add selenium action");
+            $(".menu-switch__item").click();
         });
 
-        step("- сумма кредита 2 500 000 р", () -> {
-            step("// todo: just add selenium action");
+        step("- сумма кредита 2 010 000 р", () -> {
+            //$(".slider__input").setValue("2 500 000 ₽");
+            //SelenideElement element;
+            String attribyteName = "aria-valuenow";
+            element = $(".ant-slider-handle");
+            String s;   // переменная для хранения значения элемента slider
+            s = element.getAttribute(attribyteName);
+            System.out.println("Calculation of monthly payment");
+            System.out.println(s);
+            String s1;  // вспомогательная переменная для расчета
+            s1 = determinePositionSlider(s);
+            System.out.println(s1);
+            int sm;
+            sm = Integer.parseInt(s1);
+            while (sm != 2010000) {
+                if (sm < 2010000) {
+                    element.sendKeys(Keys.ARROW_RIGHT);
+                    //slider.sendKeys(Keys.ARROW_RIGHT);
+                    s = element.getAttribute((attribyteName));
+                    s1 = determinePositionSlider(s);
+                    sm = Integer.parseInt(s1);
+                } else {
+                    element.sendKeys(Keys.ARROW_LEFT);
+                    s = element.getAttribute((attribyteName));
+                    s1 = determinePositionSlider(s);
+                    sm = Integer.parseInt(s1);
+                }
+            }
+
         });
 
         step("- Срок кредита 60 м", () -> {
-            step("// todo: just add selenium action");
+            element = $$("[inputmode='numeric']").get(1);
+            element.clear();
+            element.sendKeys("60");
         });
 
-        step("- кликнуть чекбокс «Получаю зарплату на счёт в россельхозбанке», остальные чекбоксы не должны быть активны", () -> {
-            step("// todo: just add selenium action");
-        });
-
-        step("- Сравнить что ставка = 12.4%, а ежемесячный платеж = 56118", () -> {
-            step("// todo: just add selenium action");
-        });
+        step("- проверить что значение поля 'Предварительный расчет' составляет 55 400 ₽", () -> {
+            String t;
+            t = $(".loan-calculator-result__value").text();
+            System.out.println(t);
+            $(".loan-calculator-result__value").shouldHave(text("55 400 ₽"));
+        });    
     }
+    
+    // выберем только цифры из поля убрав символы форматирования
+    public String determinePositionSlider(String s) {
+        String s1 = "";
+        char sT;
+        int j = 0, a;
+        a = s.length();
+        while (j < a) {
+            sT = s.charAt(j);
+            if (sT == '0' || sT == '1' || sT == '2' || sT == '3' || sT == '4' || sT == '5' || sT == '6' || sT == '7' ||
+                    sT == '8' || sT == '9') {
+                s1 = s1 + sT;
+            }
+            j++;
+        }
+        return s1;
+    }
+    
 }
